5db9f108841999b4e5a4011a861c364cc799b8f9
MilagrosQB <qbmilagros@gmail.com>
2016-10-31 02:26:34 -0500

    faltan detalles del responsive y el refactorizar codigo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
       $(function(){
           $("#typed").typed({
             strings: ["Bienvenidos a mi portafolio", "Mi nombre es Milagros","Soy Front-End Developer Jr", "Comunicadora Audiovisual", "MILAGROS QUISPE"],
--- js/typed.js
+++ js/typed.js
@@ -331,11 +331,3 @@
 
 }(window.jQuery);
 
-      $(function(){
-          $("#head-title").typed({
-            strings: ["Bienvenidos a mi portafolio", "Mi nombre es Milagros","Soy Front-End Developer Jr", "Comunicadora Audiovisual", "MILAGROS QUISPE"],
-            typeSpeed: 100,
-            loop: true,
-            startDelay: 100
-          });
-      });    
