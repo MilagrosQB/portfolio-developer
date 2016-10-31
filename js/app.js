cf11f87cc4935a09fd3b100cd8d234aa24bdb1aa
MilagrosQB <qbmilagros@gmail.com>
2016-10-31 16:55:35 -0500

    refactorizando codigo parte 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
       $(function(){
           $("#typed").typed({
             strings: ["Bienvenidos a mi portafolio", "Mi nombre es Milagros","Soy Front-End Developer Jr", "Comunicadora Audiovisual", "MILAGROS QUISPE"],
@@ -7,3 +6,38 @@
             startDelay: 100
           });
       });    
+
+
+function main() {
+	(function () {
+	   'use strict';
+	   
+	    $('a.page-scroll').click(function() {
+	        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	          var target = $(this.hash);
+	          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	          if (target.length) {
+	            $('html,body').animate({
+	              scrollTop: target.offset().top
+	            }, 900);
+	            return false;
+	          }
+	        }
+	      });
+	    $('body').scrollspy({ 
+	      target: '.navmenu',
+	      offset: 80,
+	    });
+
+		$(document).ready(function() {		 
+		  $("#testimonial").owlCarousel({		 
+		      navigation : false,
+		      slideSpeed : 300,
+		      paginationSpeed : 400,
+		      singleItem:true,
+		      autoHeight : true
+		  });
+		});
+	}());
+}
+main();
